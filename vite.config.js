d7abf0c1c37fe8d278dc04ab904496236f313142
MiteshBari23 <barimitesh23@gmail.com>
2025-06-15 16:20:00 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // ✅ ensure this
+  },
   server: {
     proxy: {
-      "/socket.io": {
-        target: "http://localhost:5000",
+      '/socket.io': {
+        target: 'http://localhost:5000',
         ws: true,
       },
     },
   },
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // optional, but helps
+    },
+  },
+})
